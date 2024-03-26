df6bb854fd9235d7f02c921822346e164007a601
Vamshi66767 <164719985+Vamshi66767@users.noreply.github.com>
2024-03-26 17:07:11 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,14 +1,45 @@
+#1 read text file
+from tabnanny import filename_only
 
-file1 = open("HW.txt","r")
-data = file1.read()
-print(data)
-print()
 
-file2 = open("Blank.txt","w")
-str1 = 'Python'
-file2.write(str1)
-print()
+file = open ('C:\\Users\\DELL\\OneDrive\\Desktop\\python\\vamshi.txt.txt', 'r')
+print(file.read())
 
-file3 = open("HW.txt","r+")
-print(file3.readline(11))
-print()
+#-->2)
+
+f = open('C:\\Users\\DELL\\OneDrive\\Desktop\\python\\vamshi.txt.txt','w')
+f.writelines (input())
+f.close()
+#--->3)
+try:
+    with open('C:\\Users\\DELL\\OneDrive\\Desktop\\python\\vamshi.txt.txt','r') as file:
+       for line in file:
+           print(line, end = '')
+except FileNotFoundError:
+ print("file not found")
+except IOError:
+ print("an error occured while readind the flie")
+
+#--4)
+try:
+    with open('C:\\Users\\DELL\\OneDrive\\Desktop\\python\\vamshi.txt.txt', 'rb') as file:
+     file.seek(3)
+     data = file.read(4)
+     print(data.decodes('utf-8'))
+           
+except FileNotFoundError:
+   print("file not found")
+except IOError:
+ print("an error occured while readind the flie")
+
+ import os
+def checking_permissions(filename):
+   if os.access(filename, os.R_OK):
+        print(f"file '{filename}' has read access.")
+        print(filename)
+   if os.access(filename, os.W_OK):
+        print (f"File '{filename}' has write access.")
+   else:
+       print(f"file '{filename}' has does not have access.")     
+filename = "C:\\Users\\DELL\\OneDrive\\Desktop\\python\\vamshi.txt.txt"  
+checking_permissions (filename)
